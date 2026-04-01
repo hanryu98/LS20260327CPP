00cb560482703a09fc47de65f2d74c12718e53d5
hanryu98 <hanryu98@users.noreply.github.com>
2026-04-01 13:02:37 +0900

    랜더링
    랜덤한 사각형과 색깔

--- main.cpp
+++ main.cpp
@@ -2,21 +2,94 @@
 #include "Engine.h"
 #include "World.h"
 
-#include "SDL.h"
-//사용할 라이브러리 복사
-//include 경로 추가
-//library 경로 추가
-//library 파일 등록
+#include <random>
 
-//사용할 라이브러리 파일 추가
-#pragma comment (lib, "SDL2")
-#pragma comment (lib, "SDL2main")
+#include "SDL.h"
+
+
+#pragma comment(lib, "SDL2")
+#pragma comment(lib, "SDL2main")
 
 
 using namespace std;
 
+
 int SDL_main(int argc, char* argv[])
 {
+	//rand 좀더 좋은 임의를 선택하기 위해서
+	std::random_device RandomDevice;
+	std::mt19937 RandomGenerator(RandomDevice());
+
+	std::uniform_int_distribution<int> DistX(0, 640);
+	std::uniform_int_distribution<int> DistY(0, 480);
+
+	std::uniform_int_distribution<int> DistR(0, 255);
+	std::uniform_int_distribution<int> DistG(0, 255);
+	std::uniform_int_distribution<int> DistB(0, 255);
+	std::uniform_int_distribution<int> DistA(0, 255);
+
+
+
+
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Window* MyWindow = SDL_CreateWindow("Hello", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+
+
+
+	SDL_Renderer* MyRender = SDL_CreateRenderer(MyWindow, -1, 0);
+
+	while (true)
+	{
+		SDL_Event MyEvent;
+		SDL_PollEvent(&MyEvent);
+
+		if (MyEvent.type == SDL_QUIT)
+		{
+			break;
+		}
+
+		//CPU하는건 GPU가 할일을 적는거야. 많이 많이 많이
+		//GPU 한테 보낼 명령어 모음
+		SDL_SetRenderDrawColor(MyRender, 255, 255, 255, 255);
+		SDL_RenderClear(MyRender);
+
+		//어떻게 그릴지 명령어 GPU
+		//빨간색 붓 고르고
+		for (int i = 0; i < 100; ++i)
+		{
+			SDL_SetRenderDrawColor(MyRender, DistR(RandomGenerator), DistG(RandomGenerator), DistB(RandomGenerator), DistA(RandomGenerator));
+
+			//사각형 그려
+			SDL_Rect MyRect = { DistX(RandomGenerator), DistY(RandomGenerator), DistX(RandomGenerator), DistY(RandomGenerator) };
+			SDL_RenderDrawRect(MyRender, &MyRect);
+		}
+
+
+		//그려CPU -> GPU
+		SDL_RenderPresent(MyRender);
+
+
+	}
+
+	SDL_DestroyRenderer(MyRender);
+
+	SDL_DestroyWindow(MyWindow);
+
+	SDL_Quit();
+
 	return 0;
 }
 
+//int main()
+//{
+//	GEngine->GetWorld()->Load("level01.umap");
+//
+//	GEngine->Run();
+//
+//	delete GEngine;
+//
+//	return 0;
+//}
+//
