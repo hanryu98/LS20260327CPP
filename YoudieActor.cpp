c808c77746ff88bfb838d47788d0149d8f9d4d90
hanryu98 <hanryu98@users.noreply.github.com>
2026-04-08 14:06:18 +0900

    Update YoudieActor.cpp

--- YoudieActor.cpp
+++ YoudieActor.cpp
@@ -6,7 +6,7 @@ AYoudieActor::AYoudieActor()
 	TextRenderComponent->SetText("Game Over");
 	X = 380;
 	Y = 300;
-	TextRenderComponent->ZOrder = 4000;
+	TextRenderComponent->ZOrder = 400;
 	TextRenderComponent->bIsVisible = false;
 }
 
